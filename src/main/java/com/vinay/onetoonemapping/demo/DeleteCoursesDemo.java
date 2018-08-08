49adb0e00c27efa4781c7b087dfd6ba663f655ee
VinayagamD <vinayganeshdxj@gmail.com>
2018-08-08 23:12:25 +0530

    Initial Commit Eager Vs Lazy Demo

---
+++ src/main/java/com/vinay/onetoonemapping/demo/DeleteCoursesDemo.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package com.vinay.onetoonemapping.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.vinay.onetoonemapping.entity.Course;
+import com.vinay.onetoonemapping.entity.Instructor;
+import com.vinay.onetoonemapping.entity.InstructorDetail;
+
+/**
+ * @author Dell
+ *
+ */
+public class DeleteCoursesDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").
+				addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
+				.buildSessionFactory();
+		// create session
+		Session session = factory.getCurrentSession();
+		try {
+			// Create the student object
+			System.out.println("Creating 3 student object.... ");
+			// start a transaction
+			session.beginTransaction();
+
+			// get the instructor id from the db
+			int id = 10;
+			Course course = session.get(Course.class, id);
+			System.out.println("Course : "+course);
+
+			
+			// Delete the course
+						System.out.println("Deleting the course : " + course);
+						session.delete(course);
+			
+			// commit the transaction
+			session.getTransaction().commit();
+			System.out.println("Done !");
+		} finally {
+			// add clean up code
+			session.close();
+			factory.close();
+		}
+
+	}
+
+}
