b47c3f03e3e036e50200cdb508d6992d819011b1
VinayagamD <vinayganeshdxj@gmail.com>
2018-08-10 22:55:34 +0530

    Lazy Demo

--- src/main/java/com/vinay/onetoonemapping/entity/Instructor.java
+++ src/main/java/com/vinay/onetoonemapping/entity/Instructor.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -60,6 +61,7 @@ public class Instructor implements Serializable {
 
 	
 	@OneToMany(mappedBy="instructor",
+			fetch = FetchType.LAZY,
 			cascade= {CascadeType.PERSIST, CascadeType.MERGE,
 					CascadeType.DETACH, CascadeType.REFRESH}
 			)
