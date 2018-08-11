b0687ee0ce22721ba4c721bd17c447b54c97ae92
VinayagamD <vinayganeshdxj@gmail.com>
2018-08-11 20:22:23 +0530

    Added Review Entity

--- src/main/java/com/vinay/onetoonemapping/entity/Course.java
+++ src/main/java/com/vinay/onetoonemapping/entity/Course.java
@@ -1,11 +1,13 @@
 package com.vinay.onetoonemapping.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -48,7 +50,11 @@ public class Course implements Serializable {
 	@JoinColumn(name="instructor_id")
 	private Instructor instructor;
 	
-	
+	@OneToMany(
+			fetch = FetchType.LAZY,
+			cascade= CascadeType.ALL)
+	@JoinColumn(name="course_id")
+	private List<Review> reviews;
 
 	public Course() {
 	}
@@ -79,6 +85,23 @@ public class Course implements Serializable {
 
 	public void setInstructor(Instructor instructor) {
 		this.instructor = instructor;
+	}
+	
+	
+
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+	
+//	add convenience method to add reviews
+	public void addReview(Review review) {
+		if(reviews == null)
+			reviews = new ArrayList<>();
+		reviews.add(review);
 	}
 
 	@Override
---
+++ src/main/java/com/vinay/onetoonemapping/entity/Review.java
@@ -0,0 +1,69 @@
+package com.vinay.onetoonemapping.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="review")
+public class Review implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+//	define the fields
+//	define the constructor
+//	define getter/setters
+//	define toString
+//	annotate the fields
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="comment")
+	private String comment;
+	
+	
+
+	public Review() {
+	}
+
+	public Review(String comment) {
+		this.comment = comment;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	@Override
+	public String toString() {
+		return "Review [id=" + id + ", comment=" + comment + "]";
+	}
+
+}
